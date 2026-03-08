45fcc2b79a8d83c51fdef05825ad5a576ac3d25e
Jean Jacques <rajonary.jacques@gmail.com>
2026-03-08 18:20:54 +0400

    3-modif a partir de mon mac

--- index.php
+++ index.php
@@ -5,5 +5,6 @@ echo "<p>1-Machine: Mac1</p>";
 echo "<p>Modification faite depuis le Mac 💻</p>";
 echo "<p>2-Machine: win1</p>";
 echo "<p>Modification faite depuis Windows</p>";
+echo "<p>3-Modification faite depuis mac</p>";
 
 ?>
