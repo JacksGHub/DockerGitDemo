6a609f1bdfa37de01eb676173e8aa262dcf3bde6
Jean Jacques RAJAONARY <jeanjacquesr@mccibs.ac.mu>
2026-03-08 18:11:32 +0400

    Modification depuis Windows

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 echo "<h1>Application Docker + Git</h1>";
-echo "<p>Machine: Mac1</p>";
+echo "<p>1-Machine: Mac1</p>";
 echo "<p>Modification faite depuis le Mac 💻</p>";
+echo "<p>2-Machine: win1</p>";
+echo "<p>Modification faite depuis Windows</p>";
 
 ?>
