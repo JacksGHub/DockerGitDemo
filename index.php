b2df446ae6fb7d7357ef4758f15b029740cc8e96
Jean Jacques <rajonary.jacques@gmail.com>
2026-03-08 16:54:48 +0400

    Modification index depuis Mac1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "<h1>Application Docker + Git</h1>";
-echo "<p>Machine: Windows</p>";
-echo "<p>Docker fonctionne 🚀</p>";
+echo "<p>Machine: Mac1</p>";
+echo "<p>Modification faite depuis le Mac 💻</p>";
 
 ?>
